27e132a5d772ce6fb274b3a95439ef11ea7e7770
ParkDH0809 <znxn1237@gmail.com>
2023-08-26 19:02:08 +0900

    make frame

--- tetris/src/MyPanel.java
+++ tetris/src/MyPanel.java
@@ -1,4 +1,4 @@
-package tetris;
+package tetris.src;
 
 import java.awt.*;
 import javax.swing.*;
--- tetris/src/TetrisMain.java
+++ tetris/src/TetrisMain.java
@@ -1,4 +1,4 @@
-package tetris;
+package tetris.src;
 
 public class TetrisMain {
     public static void main(String[] args) {
