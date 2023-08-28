24ce57733259cead1ba53e4d747ffc0561d27bad
ParkDH0809 <znxn1237@gmail.com>
2023-08-28 23:42:15 +0900

    블럭 추가

--- tetris/src/TetrisPanel.java
+++ tetris/src/TetrisPanel.java
@@ -1,16 +1,23 @@
 package tetris.src;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
-public class TetrisPanel extends JPanel {
+public class TetrisPanel extends JPanel implements KeyListener{
 
     final int BACKGROUND_ROWS = 20;
     final int BACKGROUND_COLS = 10;
-
+    
     JPanel tetrisPanel;
     JLabel[][] gameLabel = new JLabel[BACKGROUND_ROWS][BACKGROUND_COLS];
+
+    int blockX = 140, blockY = 5;
+    int[][] currentShape = {{1, 1}, {1, 1}};
+
 
     TetrisPanel() {
         tetrisPanel = new JPanel(new GridLayout(BACKGROUND_ROWS, BACKGROUND_COLS));
@@ -18,8 +25,11 @@ public class TetrisPanel extends JPanel {
         setOpaque(false);
 
         makeTetrisBackground();
-
         add(tetrisPanel);
+
+
+
+        addBlock();
     }
 
     void makeTetrisBackground() {
@@ -33,7 +43,83 @@ public class TetrisPanel extends JPanel {
                 
                 tetrisPanel.add(gameLabel[i][j]);
             }
-
         }
     }
+
+
+
+    //block을 그리는 메서드
+    @Override
+    public void paint(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paint(g);
+        for (int row = 0; row < currentShape.length; row++) {
+            for (int col = 0; col < currentShape[row].length; col++) {
+                if (currentShape[row][col] == 1) {
+                    g.fillRect((blockX + col), (blockY + row), 30, 30);
+                }
+            }
+        }
+    }
+
+
+    void addBlock() {
+        Block b = new Block();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_LEFT) {// 왼쪽 화살표 키 처리
+            if (canMoveTo(blockX - 1, blockY)) {
+                blockX--;
+            }
+        } else if (keyCode == KeyEvent.VK_RIGHT) {// 오른쪽 화살표 키 처리
+            if (canMoveTo(blockX + 1, blockY)) {
+                blockX++;
+            }
+        } else if (keyCode == KeyEvent.VK_DOWN) {// 아래 화살표 키 처리
+            if (canMoveTo(blockX, blockY + 1)) {
+                blockY++;
+            } else {
+                // mergeShapeToBoard();
+                // checkAndClearLines();
+                // createNewShape();
+                // blockX = BOARD_WIDTH / 2 - 1;
+                // blockY = 0;
+            }
+        }
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+    private boolean canMoveTo(int x, int y) {
+
+        return true;
+    }
+}
+
+
+class Block {
+    int type;
+    int[][] block = {
+        {1, 1},
+        {1, 1}
+    };
+
+    
+
 }
